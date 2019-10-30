6295636ab787745212fe360f2c675602d56ed3ab
fivenull <fivenull@163.com>
2019-10-30 02:44:09 +0000

    用户可以上传话题图片

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -20,7 +20,9 @@ class TopicRequest extends Request
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
