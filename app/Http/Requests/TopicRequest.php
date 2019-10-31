ef4944832a8dc5ec708c66b6f5d2bbf2c90df9ec
fivenull <fivenull@163.com>
2019-10-31 01:50:56 +0000

    帖子回复

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -6,32 +6,9 @@ class TopicRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    'title'       => 'required|min:2',
-                    'body'        => 'required|min:3',
-                    'category_id' => 'required|numeric',
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            }
-        }
+        return [
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
