b58f5f45240e198b7c122623961b7c657363990e
fivenull <fivenull@163.com>
2019-11-07 03:47:50 +0000

    安装 Passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -32,5 +33,13 @@ class AuthServiceProvider extends ServiceProvider
             // 动态返回模型对应的策略名称，如：// 'App\Model\User' => 'App\Policies\UserPolicy',
             return 'App\Policies\\'.class_basename($modelClass).'Policy';
         });
+        // passport route
+        Passport::routes();
+        // access_token expiration time
+        Passport::tokensExpireIn(now()->addDays(15));
+        // refreshTokens expiration time
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+
+
     }
 }
